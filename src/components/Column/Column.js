f59ac1ed84873e7e9f2ad79ccf99feb4abae08bf
Maciej Kryczka <maciejkryczka@hotmail.com>
2019-09-27 21:49:15 +0200

    change Column component from arrow function to class

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
+import Card from '../Card/Card';
 
-const Column = argument => (
+class Column extends React.Component {
+    state = {
+        cards: this.props.cards || [],
+      }
 
+    static propTypes = {
+        cards: PropTypes.array,
+        column: PropTypes.array,
+    }
+
+    render(){
+        return (
     <section className={styles.component}>
-        <h3 className={styles.title}>{argument.title}</h3>
+        <h3 className={styles.title}>{this.props.title}</h3>
     </section>
-);
+        )
+    }
 
-Column.propTypes = {
-    title: PropTypes.string,
 }
 
 
